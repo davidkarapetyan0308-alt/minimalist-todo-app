58f6984c1df8752593aa39fcdb67639127b756e5
David <davidkarapetyan0308@gmail.com>
2026-03-01 15:21:58 +0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `priority` enum('low','medium','high') COLLATE utf8mb4_unicode_ci DEFAULT 'low',
+  `status` enum('todo','done') COLLATE utf8mb4_unicode_ci DEFAULT 'todo',
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
